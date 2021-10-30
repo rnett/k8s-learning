a447cf12d4e1c42b528647272948c9611091a490
Ryan Nett <JNett96@gmail.com>
2021-10-29 21:32:37 -0700

    Make the plugin work

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "1.5.31" apply false
     id("com.bmuschko.docker-java-application") version "7.1.0" apply false
     id("org.unbroken-dome.helm") version "1.6.1"
+    id("org.unbroken-dome.helm-releases") version "1.6.1"
 }
 
 allprojects {
@@ -19,11 +20,31 @@ allprojects {
     }
 }
 
-helm{
-//    namespace.set(project.name)
-    xdgCacheHome.set(file("${project.buildDir}/helm/cache"))
+helm {
+    namespace.set(project.name)
+
+    repositories {
+        bitnami()
+    }
+
     downloadClient {
         enabled.set(true)
         version.set("3.7.0")
+    }
+
+    val main by charts.creating {
+        sourceDir.set(file("${project.projectDir}/src/main/helm"))
+    }
+    releases {
+        create("main") {
+            from(main)
+            namespace.set(this@helm.namespace)
+        }
+    }
+    releaseTargets {
+        create("local") {
+//            kubeContext.set("docker-for-desktop")
+        }
+        activeReleaseTarget.set("local")
     }
 }
