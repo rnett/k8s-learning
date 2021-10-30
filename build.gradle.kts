c02ac1305602201c615a2273c2f37ebe4303d53f
Ryan Nett <JNett96@gmail.com>
2021-10-29 21:14:59 -0700

    Basic helm chart, not working plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     kotlin("jvm") version "1.5.31" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.5.31" apply false
     id("com.bmuschko.docker-java-application") version "7.1.0" apply false
+    id("org.unbroken-dome.helm") version "1.6.1"
 }
 
 allprojects {
@@ -15,5 +16,14 @@ allprojects {
 
     repositories {
         mavenCentral()
+    }
+}
+
+helm{
+//    namespace.set(project.name)
+    xdgCacheHome.set(file("${project.buildDir}/helm/cache"))
+    downloadClient {
+        enabled.set(true)
+        version.set("3.7.0")
     }
 }
